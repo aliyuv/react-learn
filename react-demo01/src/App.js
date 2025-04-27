8eadc6a239312f25753ab38ed60cbd37a2372b4e
aliyuv <1324149139@qq.com>
2025-04-27 23:57:32 +0800

    add: 从底层模式修改为  使用 vite 来构建 react 项目

--- react-demo01/src/App.js
+++ react-demo01/src/App.js
@@ -1,36 +1,39 @@
+import React from "react"
+import { createRoot } from "react-dom/client"
+
 const Demo01 = (props) => {
   return React.createElement(
-      'div',
-      {},
-      [
-          React.createElement('h1', {}, props.name),
-          React.createElement('p', {}, props.description)
-      ]
+    'div',
+    {},
+    [
+      React.createElement('h1', {}, props.name),
+      React.createElement('p', {}, props.description)
+    ]
   )
 }
 
 const App = () => {
   return React.createElement('div', {}, [
-      React.createElement('div', {}, '这里是主页组件'),
-      React.createElement(Demo01, {
-          name: '你好世界',
-          description: '这是一条描述信息'
-      }),
-      React.createElement(Demo01, {
-          name: '你好世界',
-          description: '这是一条描述信息'
-      }),
-      React.createElement(Demo01, {
-          name: "The Hawaiian Pizza",
-          description: "Sliced Ham, Pineapple, Mozzarella Cheese",
-      }),
-      React.createElement(Demo01, {
-          name: "The Hawaiian Pizza",
-          description: "Sliced Ham, Pineapple, Mozzarella Cheese",
-      })
+    React.createElement('div', {}, '这里是主页组件'),
+    React.createElement(Demo01, {
+      name: '你好世界',
+      description: '这是一条描述信息'
+    }),
+    React.createElement(Demo01, {
+      name: '你好世界',
+      description: '这是一条描述信息'
+    }),
+    React.createElement(Demo01, {
+      name: "The Hawaiian Pizza",
+      description: "Sliced Ham, Pineapple, Mozzarella Cheese",
+    }),
+    React.createElement(Demo01, {
+      name: "The Hawaiian Pizza",
+      description: "Sliced Ham, Pineapple, Mozzarella Cheese",
+    })
   ])
 }
 
 const container = document.getElementById('root')
-const root = ReactDOM.createRoot(container)
+const root = createRoot(container)
 root.render(React.createElement(App))
