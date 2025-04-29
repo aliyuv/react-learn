2d8cd507c194ade3e4a3e7e2bbc75f2662b19978
aliyuv <1324149139@qq.com>
2025-04-29 14:09:46 +0800

    feat: 添加 Order 组件并更新样式 - 新增 Order.jsx 组件 - 更新 App.jsx 中的组件引用 - 优化 CSS 样式

--- react-pizza/src/App.jsx
+++ react-pizza/src/App.jsx
@@ -1,31 +1,11 @@
 import React from "react"
 import { createRoot } from "react-dom/client"
-import Pizza from "./Pizza"
-
+import Order from "./Order"
 const App = () => {
   return (
     <div>
       <h1 className="logo">Padre Gino's Pizza</h1>
-      <Pizza
-        name="Pepperoni"
-        description="Mozzarella Cheese, Pepperoni"
-        image={"/public/pizzas/bbq_ckn.webp"}
-      />
-      <Pizza
-        name="Margherita"
-        description="Mozzarella Cheese, Tomato Sauce, Basil"
-        image={"/public/pizzas/ckn_alfredo.webp"}
-      />
-      <Pizza
-        name="Vegetarian"
-        description="Mozzarella Cheese, Tomato Sauce, Mushrooms, Onions, Peppers"
-        image={"/public/pizzas/ckn_pesto.webp"}
-      />
-      <Pizza
-        name="Hawaiian"
-        description="Mozzarella Cheese, Tomato Sauce, Ham, Pineapple"
-        image={"/public/pizzas/ckn_pesto.webp"}
-      />
+      <Order />
     </div>
   )
 }
