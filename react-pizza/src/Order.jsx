b05a8ea2553ba7ff442077cfd6572d9431788152
aliyuv <1324149139@qq.com>
2025-04-30 14:22:37 +0800

    重构 order 组件 封装异步请求 展示pizza

--- react-pizza/src/Order.jsx
+++ react-pizza/src/Order.jsx
@@ -1,9 +1,38 @@
-import { useState } from "react"
-import Pizza from "./Pizza"
+import { useState, useEffect } from 'react'
+import Pizza from './Pizza'
+
+// feel free to change en-US /usd to your locale
+const intl = new Intl.NumberFormat('en-US', {
+  style: 'currency',
+  currency: 'USD'
+})
 
 export default function Order() {
   const [pizzaType, setPizzaType] = useState('pepperoni')
   const [pizzaSize, setPizzaSize] = useState('M')
+  const [pizzaTypes, setPizzaTypes] = useState([])
+  const [loading, setLoading] = useState(true)
+
+  let price, selectedPizza
+  if (!loading) {
+    selectedPizza = pizzaTypes.find(pizza => pizzaType === pizza.id)
+    price = intl.format(selectedPizza?.sizes[pizzaSize])
+  }
+
+  useEffect(() => {
+    fetchPizzaTypes()
+  }, [])
+
+  async function fetchPizzaTypes() {
+    await new Promise((resolve) => setTimeout(resolve, 3000))
+    const pizzaRes = await fetch('/api/pizzas')
+    const pizzaJson = await pizzaRes.json()
+    setPizzaTypes(pizzaJson)
+    setLoading(false)
+  }
+
+
+
   return (
     <div className="order">
       <h2>Create Order</h2>
@@ -12,9 +41,13 @@ export default function Order() {
           <div>
             <label htmlFor="pizza-type">Pizza Type</label>
             <select name="pizza-type" value={pizzaType} onChange={e => setPizzaType(e.target.value)}>
-              <option value="pepperoni">The Pepperoni Pizza</option>
-              <option value="hawaiian">The Hawaiian Pizza</option>
-              <option value="big_meat">The Big Meat Pizza</option>
+              {
+                pizzaTypes.map(pizza =>
+                  <option key={pizza.id} value={pizza.id}>
+                    {pizza.name}
+                  </option>
+                )
+              }
             </select>
           </div>
           <div>
@@ -57,13 +90,20 @@ export default function Order() {
           </div>
           <button type="submit">Add to Cart</button>
         </div>
-        <div className="order-pizza">
-          <Pizza
-            name="Pepperoni"
-            description="Mozzarella Cheese, Pepperoni"
-            image="/public/pizzas/pepperoni.webp"
-          />
-        </div>
+        {
+          loading ? (
+            <h3>Loading...</h3>
+          ) : (
+            <div className="order-pizza">
+              <Pizza
+                name={selectedPizza.name}
+                description={selectedPizza.description}
+                image={selectedPizza.image}
+              />
+              <p>{price}</p>
+            </div>
+          )
+        }
       </form>
     </div>
   )
